ba9ff13ed768067c1e07dcf20c6a0e00ad3641eb
Fran <francisco.chiarino@gmail.com>
2021-12-05 16:33:05 +0100

    Create avatar field for users
    - Install image processing
    - Validate email and password only on create
    - Render errors on forms if any

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,45 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new(create_user_params)
     byebug
 
     if @user.save
       flash[:notice] = 'User created'
       redirect_to login_path
     else
-      render 'new'
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(edit_user_params)
+      redirect_to @user
+    else
+      render :edit
     end
   end
 
   private
 
-  def user_params
+  def create_user_params
     params.require(:user).permit(:email, :password)
+  end
+
+  def edit_user_params
+    params.require(:user).permit(:first_name, :last_name, :avatar)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_one_attached :avatar
 
-  validates :email, presence: true, uniqueness: true
-  validates :password, length: { minimum: 8 }
+  validates :email, presence: true, uniqueness: true, on: :create
+  validates :password, length: { minimum: 8 }, on: :create
 end
